b61bb26b95d2a500f693a8f21e5e69ea400ea55d
Julio Cámara <jjcamarat@gmail.com>
2019-08-28 21:45:10 -0500

    Initial Commit

---
+++ GitPrueba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitPrueba
+//
+//  Created by Julio Camara on 8/28/19.
+//  Copyright © 2019 Julio Camara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
